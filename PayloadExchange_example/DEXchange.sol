1877aa79faf6c2d225bcc65b6f5751f5ffc6d72d
Dexaran <dexaran820@gmail.com>
2017-03-22 11:33:32 +0400

    Comments added

--- PayloadExchange_example/DEXchange.sol
+++ PayloadExchange_example/DEXchange.sol
@@ -35,13 +35,17 @@ contract DEXchange {
     address public token1Owner;
     address public token2Owner;
     
+    
+    //Here you can enter a string name of method you need to execute
+    //and receive its signer.
     function whatTheHash(string _src) constant returns (bytes4) {
         return bytes4(sha3(_src));
     }
     
+    //Token fallback function that will accept supported token1 or token2
+    //and reject any other token that is not supported.
     function tokenFallback(address _from, uint _value, bytes _data) returns (bool result) 
     {
-        
         if(msg.sender==token1Contract) {
             token1Owner = _from;
             token1Balance = _value;
@@ -51,10 +55,15 @@ contract DEXchange {
             token2Owner = _from;
             token2Balance = _value;
         }
+        
+        //reject every unsupported incoming token
         else {
             throw;
         }
         
+        //Trade happens only if `trade` is a signer of incoming token transaction
+        //otherwise token transaction will be rejected if the data is incorrect
+        //or tokens will be stored inside contract if the data is empty.
         if(_data.length!=0) {
             ERC23 token1 = ERC23(token1Contract);
             ERC23 token2 = ERC23(token2Contract);
@@ -62,12 +71,15 @@ contract DEXchange {
             signer = signerFromData(_data);
             
             if(signer==bytes4(sha3("trade"))) {
+                //Token contracts can be called here.
                 token1.transfer(token2Owner, token2Balance);
                 token2.transfer(token1Owner, token1Balance);
             }
-            //throw;
         }
     }
+    
+    //First four bytes are function signer
+    //similar to Ether function calling signer
     
     function signerFromData(bytes _data) private returns (bytes4) {
         uint32 u = uint32(_data[3]) + (uint32(_data[2]) << 8) + (uint32(_data[1]) << 16) + (uint32(_data[0]) << 24);
@@ -75,6 +87,7 @@ contract DEXchange {
         return sig;
     }
     
+    //Interprets bytes arg intor a single uint256 variable.
     function parseSingleUintArg(bytes _data) private returns (uint) {
         uint x = 0;
         for (uint i = 0; i < 32; i++) {
