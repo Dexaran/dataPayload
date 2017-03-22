5594633e88f2d61af3f3860a13eeeebecee4978f
Dexaran <dexaran820@gmail.com>
2017-03-22 11:34:41 +0400

    Update ERC23Asset.sol

--- PayloadExchange_example/ERC23Asset.sol
+++ PayloadExchange_example/ERC23Asset.sol
@@ -3,23 +3,15 @@ pragma solidity ^0.4.9;
  /* New ERC23 contract interface */
  
  contract contractReceiver{
-     function tokenFallback(address, uint, bytes)
-     {
-         
-     }
+     function tokenFallback(address, uint, bytes) returns (bool);
  }
 
 contract ERC23 {
   uint public totalSupply;
   function balanceOf(address who) constant returns (uint);
-  function allowance(address owner, address spender) constant returns (uint);
-
+  
   function transfer(address to, uint value) returns (bool ok);
   function transfer(address to, uint value, bytes data) returns (bool ok);
-  function transferFrom(address from, address to, uint value) returns (bool ok);
-  function approve(address spender, uint value) returns (bool ok);
-  event Transfer(address indexed from, address indexed to, uint value);
-  event Approval(address indexed owner, address indexed spender, uint value);
 }
 
 
